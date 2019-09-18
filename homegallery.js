0bc687d379ffc91e8909be0ab7b09c4109c8ecee
GouravSaini125 <gdinsan777@gmail.com>
2019-09-18 16:32:50 +0530

    hh

--- homegallery.js
+++ homegallery.js
@@ -1,7 +1,7 @@
 
 $(function() {
     var options = '';
-    for (var i = 1; i < 27; i++) {
+    for (var i = 1; i < 9; i++) {
         options += '<div class="test"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
       }
     $('#gallery').html(options);
