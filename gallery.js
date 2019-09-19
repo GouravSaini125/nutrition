ec45f189b931da5ef0621c74018ce2d9650a15ed
GouravSaini125 <gdinsan777@gmail.com>
2019-09-19 22:45:38 +0530

    comp

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@
 $(function() {
     var options = '';
     for (var i = 1; i < 20; i++) {
-        options += '<div class="test"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+        options += '<div class="test" data-aos="zoom-in"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
       }
     $('#gallery').html(options);
     
--- pamphlet.js
+++ pamphlet.js
@@ -1,7 +1,18 @@
-$(function() {
+var j = 1;
+
+$(function () {
     var options = '';
-    for (var i = 22; i < 26; i++) {
-        options += '<div class="test"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+  for (var i = 22; i < 26; i++) {
+    if (i % 2 == 0)
+      if (j == 0)
+        j = 1;
+      else
+        j = 0;
+      if(j==1)
+        options += '<div class="test" data-aos="fade-left"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+      else
+        options += '<div class="test" data-aos="fade-right"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+    
       }
     $('#gallery').html(options);
     
@@ -10,8 +21,16 @@ $(function() {
 
   $(function() {
     var options = '';
-    for (var i = 26; i < 62; i++) {
-        options += '<div class="test"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+    for (var i = 26; i <= 82; i++) {
+      if (i % 2 == 0)
+      if (j == 0)
+        j = 1;
+      else
+        j = 0;
+            if(j==1)
+              options += '<div class="test" data-aos="fade-left"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
+            else
+              options += '<div class="test" data-aos="fade-right"><a href="img/' + i + '.jpg" data-toggle="lightbox" data-gallery="image"><img src="img/' + i + '.jpg" class="img-fluid"></a></div>';
       }
     $('#news').html(options);
     
